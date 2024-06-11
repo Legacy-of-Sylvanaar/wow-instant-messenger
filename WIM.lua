c8851b365d2a1d42d958e5522e3dd4ea33cdb340
John Langone <johnlangone@gmail.com>
2024-06-11 18:19:39 -0400

    Remove dependency of LibChatHandler-1.0.

--- WIM.lua
+++ WIM.lua
@@ -64,7 +64,6 @@ local Events = {};
 
 -- import libraries.
 libs.SML = _G.LibStub:GetLibrary("LibSharedMedia-3.0");
-libs.ChatHandler = _G.LibStub:GetLibrary("LibChatHandler-1.0");
 libs.DropDownMenu = _G.LibStub:GetLibrary("LibDropDownMenu");
 
 -- called when WIM is first loaded into memory but after variables are loaded.
