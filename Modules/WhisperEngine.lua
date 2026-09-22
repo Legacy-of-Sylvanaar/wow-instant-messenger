4cafa1a446cff86d9cd38a16a146373d72374753
John Langone <johnlangone@gmail.com>
2026-09-22 15:31:04 -0400

    Fix: If using regional unique names, don't strip out spaces our of a player's name.

--- Modules/WhisperEngine.lua
+++ Modules/WhisperEngine.lua
@@ -254,8 +254,10 @@ local function getWhisperWindowByUser(user, isBN, bnID, fromEvent)
 			return bnWin;
 		end
 	else
-		user = string.gsub(user," ","") -- Drii: WoW build15050 whisper bug for x-realm server with space
-	    user = fromEvent and user or FormatUserName(user);
+		if not _G.RegionalUniqueNamesEnabled or not _G.RegionalUniqueNamesEnabled() then
+			user = string.gsub(user," ","") -- Drii: WoW build15050 whisper bug for x-realm server with space
+	    end
+		user = fromEvent and user or FormatUserName(user);
 	end
 
     if(not user or user == "") then
